7eaa8c289918ed1dd6371daab87c7392a3420053
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2020-03-04 22:55:36 -0500

    building a full managed plugin example

---
+++ VisualStudio/ChuckNorris.UnityFull/JokeSetter.cs
@@ -0,0 +1,34 @@
+﻿using TMPro;
+
+using UnityEngine;
+
+namespace ChuckNorris
+{
+    public class JokeSetter : MonoBehaviour
+    {
+        public TextMeshProUGUI text;
+        private ChuckNorris client;
+
+        private void OnValidate()
+        {
+            if (text == null)
+            {
+                text = GetComponent<TextMeshProUGUI>();
+            }
+        }
+
+        private void Awake()
+        {
+            client = new ChuckNorris();
+        }
+
+        public void NextJoke()
+        {
+            if (text != null)
+            {
+                text.SetText(client.GetRandom().value);
+            }
+        }
+    }
+
+}
